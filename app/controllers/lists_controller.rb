7ecb7e3616a49e6e3976cac3b4b800bcde0326f6
Liz Tom & Anne Jhetson <student@epicodus.com>
2014-03-31 20:00:27 -0700

    add task to list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,7 +5,7 @@ class ListsController < ApplicationController
   end
 
   def new
-    @list = List.new
+    @list = List.new()
     render('lists/new.html.erb')
   end
 
@@ -14,4 +14,30 @@ class ListsController < ApplicationController
     @tasks = @list.show_tasks
     render('lists/show.html.erb')
   end
+
+  def create
+    @list = List.new(:name => params[:name])
+     if @list.save
+       render('lists/success.html.erb')
+     else
+       render('lists/new.html.erb')
+      end
+  end
+
+  def new_task
+    @list = List.find(params[:id])
+
+    render('lists/add_task.html.erb')
+  end
+
+  def add_task
+    @list = List.find(params[:id])
+    @list.tasks.new(name: params[:name],
+                     due_date: params[:due_date],
+                     list_id: params[:list_id])
+    @list.save
+    render('lists/success.html.erb')
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Todo::Application.routes.draw do
   match('lists', {via: :get, to: 'lists#index'})
+  match('lists', {via: :post, to: 'lists#create'})
+  match('lists/new', {via: :get, to: 'lists#new'})
   match('lists/:id', {via: :get, to: 'lists#show'})
 
-  match('tasks', {via: :get, to: 'tasks#index'})
+  match('lists/:id', {via: :post, to: 'lists#add_task'})
+  match('lists/:id/add_task', {via: :get, to: 'lists#new_task'})
+
+
+
+  # match('tasks', {via: :get, to: 'tasks#index'})
   # match('tasks', {via: :post, to: 'tasks#create'})
-  match('tasks/new', {via: :get, to: 'tasks#new'})
-  match('tasks/:id', {via: :get, to: 'tasks#show'})
-  match('tasks/:id/edit', {via: :get, to: 'tasks#edit'})
-  match('tasks/:id', {via: [:patch, :put], to: 'tasks#update'})
-  match('tasks/:id', {via: :delete, to: 'tasks#destroy'})
-  match('lists/:list_id/tasks', {via: :post, to: 'tasks#create'})
+  # match('tasks/new', {via: :get, to: 'tasks#new'})
+  # match('tasks/:id', {via: :get, to: 'tasks#show'})
+  # match('tasks/:id/edit', {via: :get, to: 'tasks#edit'})
+  # match('tasks/:id', {via: [:patch, :put], to: 'tasks#update'})
+  # match('tasks/:id', {via: :delete, to: 'tasks#destroy'})
+  # match('lists/:list_id/tasks', {via: :post, to: 'tasks#create'})
 end
